10c5c6dd90a0af6726ebf97d0ba76117d5d454f4
aslisabanci <aslisabanci@gmail.com>
2020-10-02 09:11:09 -0700

    Renamed model config as model manifest

--- YOUR_ALGORITHM_NAME.py
+++ YOUR_ALGORITHM_NAME.py
@@ -2,11 +2,15 @@ import Algorithmia
 import json
 import os.path
 import joblib
+import xgboost
+import pandas as pd
+import hashlib
+
 
 client = Algorithmia.client()
 
 
-def load_model_config(config_rel_path="model_config.json"):
+def load_model_manifest(rel_path="model_manifest.json"):
     """Loads the model manifest file as a dict. 
     A manifest file has the following structure:
     {
@@ -19,36 +23,51 @@ def load_model_config(config_rel_path="model_config.json"):
       "model_uploaded_utc": UTC timestamp of the automated model upload
     }
     """
-    config = []
-    config_path = "{}/{}".format(os.getcwd(), (config_rel_path))
-    if os.path.exists(config_path):
-        with open(config_path) as json_file:
-            config = json.load(json_file)
-    return config
+    manifest = []
+    manifest_path = "{}/{}".format(os.getcwd(), (rel_path))
+    if os.path.exists(manifest_path):
+        with open(manifest_path) as json_file:
+            manifest = json.load(json_file)
+    return manifest
 
 
-def load_model(config):
+def load_model(manifest):
     """Loads the model object from the file at model_filepath key in config dict"""
-    model_path = config["model_filepath"]
-    try:
-        if __name__ == "__main__":
-            # Loading the model file as usual, since we're working with the local model file in this template
-            return joblib.load(model_path)
-            # However, you can still load a model from an Algorithmia data collection if you initialize Algorithmia client with your API key.
-        else:
-            return joblib.load(client.file(model_path).getFile().name)
-    except Exception as e:
-        print("Exception occurred while loading the model file: {}".format(e))
-        return None
+    model_path = manifest["model_filepath"]
+    if __name__ == "__main__":
+        model_file = model_path
+    else:
+        model_file = client.file(model_path).getFile().name
+        assert_model_md5(model_file)
+    model_obj = joblib.load(model_file)
+    return model_obj
 
 
-config = load_model_config()
-model = load_model(config)
+def assert_model_md5(model_file):
+    """
+    Calculates the loaded model file's MD5 and compares the actual file hash with the hash on the model manifest
+    """
+    md5_hash = None
+    DIGEST_BLOCK_SIZE = 128 * 64
+    with open(model_file, "rb") as f:
+        hasher = hashlib.md5()
+        buf = f.read(DIGEST_BLOCK_SIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = f.read(DIGEST_BLOCK_SIZE)
+        md5_hash = hasher.hexdigest()
+    assert manifest["model_md5_hash"] == md5_hash
+    print("Model file's runtime MD5 hash equals to the upload time hash, great!")
+
+
+manifest = load_model_manifest()
+model = load_model(manifest)
 
 
 # API calls will begin at the apply() method, with the request body passed as 'input'
 # For more details, see algorithmia.com/developers/algorithm-development/languages
 def apply(input):
+    # You can access the model object from this scope
     return f"Echoing back input: {input}"
 
 
